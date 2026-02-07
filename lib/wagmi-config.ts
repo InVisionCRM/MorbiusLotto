0a5fcff60592f4d1d7159c4c54bcb050c347a19f
Kcruiser <handymandet@gmail.com>
2026-02-06 21:06:28 -0500

    WERWRTW

--- lib/wagmi-config.ts
+++ lib/wagmi-config.ts
@@ -8,27 +8,10 @@ import {
   rainbowWallet,
 } from '@rainbow-me/rainbowkit/wallets'
 
-const internetMoneyWallet: typeof walletConnectWallet = ({ projectId, options }) => {
-  const wcWallet = walletConnectWallet({ projectId, options })
-  return {
-    ...wcWallet,
-    id: 'internetmoney',
-    name: 'Internet Money',
-    iconUrl: 'https://internetmoney.io/images/logo.png',
-    iconBackground: '#000000',
-    downloadUrls: {
-      android: 'https://play.google.com/store/apps/details?id=io.internetmoney.app',
-      ios: 'https://apps.apple.com/app/internet-money-wallet/id6443579962',
-      qrCode: 'https://internetmoney.io',
-    },
-    mobile: {
-      getUri: (uri: string) => `internetmoney://wc?uri=${encodeURIComponent(uri)}`,
-    },
-    qrCode: {
-      getUri: (uri: string) => uri,
-    },
-  }
-}
+const injectedWalletRenamed: typeof injectedWallet = (params) => ({
+  ...injectedWallet(params),
+  name: 'Injected',
+})
 
 const connectors = connectorsForWallets(
   [
@@ -36,8 +19,7 @@ const connectors = connectorsForWallets(
       groupName: 'Popular',
       wallets: [
         metaMaskWallet,
-        internetMoneyWallet,
-        injectedWallet,
+        injectedWalletRenamed,
         walletConnectWallet,
         rainbowWallet,
       ],
