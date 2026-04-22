518b32e2c6dd1c3afce71eefd8210cb11b142b70
Kcruiser <handymandet@gmail.com>
2026-04-22 10:16:13 -0400

    NEW Stuff

--- components/home/dev-log-section.tsx
+++ components/home/dev-log-section.tsx
@@ -9,7 +9,7 @@ const panelShell =
 
 const panelInset = {
   boxShadow:
-    'inset 0 3px 6px rgba(0, 0, 0, 0.8), inset 0 -3px 6px rgba(255, 255, 255, 0.1), 0 1px 3px rgba(0, 0, 0, 0.5)',
+    'inset 0 3px 6px rgba(104, 38, 38, 0.8), inset 0 -3px 6px rgba(255, 255, 255, 0.1), 0 1px 3px rgba(0, 0, 0, 0.5)',
 } as const
 
 function XLogo({ className }: { className?: string }) {
